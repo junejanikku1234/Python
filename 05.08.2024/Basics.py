515b41a8b7cc8ae30c95018892898c1110adfbb6
junejanikku1234 <23csu215@ncuinida.edu>
2024-08-05 16:06:29 +0530

    boolean and floor operators added

--- 05.08.2024/Basics.py
+++ 05.08.2024/Basics.py
@@ -9,4 +9,24 @@ print(b+c)
 print(type(b+c))
 comp = 5+6j
 s = complex(input("Enter a complex num to multiply with: "))
-print(comp," multiplied by ",s,"is: ",comp*s)
+print(comp," added to ",s,"is: ",comp+s)
+precedent = True
+choice = bool(input("Enter your choice(True/False): "))
+if (choice == precedent):
+    print("Your choice matches societal precedent.")
+else:
+    print("Your choice doesn't match societal precedent.")
+a = 10
+b = 3
+c = 2
+result1 = a / b // c  # (10 / 3) // 2 = 3.3333 // 2 = 1.0
+print(f"Result of a / b // c:", result1)
+a = 5
+b = 10
+c = 3
+d = 2
+e = True
+f = False
+result11 = not e or f and e
+result5 = a * b / c // d % 3
+print(result11,result5)
