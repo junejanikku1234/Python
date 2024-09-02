4666c19a20eaad75112989a35b7b06f2dd9e3841
junejanikku1234 <23csu215@ncuinida.edu>
2024-09-02 16:09:12 +0530

    did exp5

--- 02.05.2024/exp4.py
+++ 02.05.2024/exp4.py
@@ -1,9 +1,9 @@
 def checkPrime(n):
-    if (n<=1):
+    if n<=1:
         return False
     i = 2
     while (i<=n-1):
-        if (n%i==0):
+        if n%i==0:
             return False
         i += 1
     return True
---
+++ 02.05.2024/exp5.py
@@ -0,0 +1,15 @@
+import math
+
+if __name__ == '__main__':
+    print(math.factorial(5))
+    print(math.ceil(4.78))
+    print(math.floor(6.98))
+    print(math.sqrt(625))
+    print(math.sqrt(625))
+    print(math.pow(2, 3))
+    print(math.log(16, 4))  #specify base
+    print(math.gcd(4,16))
+    print(math.log(2.718))  #default base e
+    print(math.sin(math.radians(90)))
+    print(math.fabs(-90))
+    print(math.asinh(-0.5))
