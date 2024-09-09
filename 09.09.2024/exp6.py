7e2623310995d10012b5abd98b512f6ecc0f19bc
junejanikku1234 <23csu215@ncuinida.edu>
2024-09-09 15:39:02 +0530

    exp 6 done

--- 09.09.2024/exp6.py
+++ 09.09.2024/exp6.py
@@ -1,2 +1,23 @@
 if __name__ == "__main__":
-    print("Hello World!")
+    print("I only enjoy window shopping when the windows are transparent.".upper())
+    print("The fox in the tophat whispered into the ear of the rabbit.".lower())
+    print("While on the first date he accidentally hit his head on the beam.".split())
+    print("I cheated while playing the darts tournament by using a longbow.".title())
+    print(len("They were excited to see their first sloth."))
+    print("Nancy was proud that she ran a tight shipwreck.".isalnum())
+    print("my name is richard".capitalize())
+    print("He told us a very exciting adventure story.".count("a"))
+    print("Instead of a bachelor party".replace("e","X",2))
+    print("Choosing to do nothing is still a choice, after all."[-8:-1:3])
+    print("Patricia found the meaning of life in a bowl of Cheerios.".startswith("X"))
+    print("Sit ups are a terrible way to end your day".swapcase())
+    print("X".join("I love you.".split()))
+    print("He embraced his new life as an eggplant.".isnumeric())
+    print("Homesickness became contagious in the young campers' cabin.".endswith("."))
+    print(" ".isspace())
+    print("       I love junk food.".lstrip())
+    print("Whatever you want to do, you can.".find("y"))
+    print("Whatever you want to do, you can.".index("y"))
+    print("Whatever you want to do, you can.".find("H")) # returns -1 if substring not found
+    #print("Whatever you want to do, you can.".index("H")) # returns ValueError if substring not found
+
