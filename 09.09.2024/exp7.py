1ea4465f8907a532ea7961f759e4986fe687a7a9
junejanikku1234 <23csu215@ncuinida.edu>
2024-09-16 15:53:56 +0530

    added keyword arguments example

--- 09.09.2024/exp7.py
+++ 09.09.2024/exp7.py
@@ -6,7 +6,15 @@ def factorial(n):
     else:
         return n * factorial(n-1)
 
+def my_function(a, b, c=10, d=20):
+    print(a, b, c, d)
+
 if __name__ == '__main__':
     print(factorial(5))
     print(factorial(-12))
     print(factorial(0))
+
+    my_function(1, 2)
+    my_function(1, 2, 3)
+    my_function(1, 2, 3, 4)
+    my_function(d=20, c=10, a=1, b=2)
