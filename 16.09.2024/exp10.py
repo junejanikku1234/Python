d579bf011f7f6debb4c7efd0a48f05d9c72eba7c
junejanikku1234 <23csu215@ncuinida.edu>
2024-09-16 15:47:16 +0530

    exp10 tups

---
+++ 16.09.2024/exp10.py
@@ -0,0 +1,9 @@
+comp_part_tup = ("CPU", "GPU", "RAM", "SSD", "Motherboard")
+
+print(comp_part_tup)
+
+tup_part2 = ("PSU", "Mouse", "Keyboard", "NIC", "Sound card")
+
+joined_tup = comp_part_tup + tup_part2
+
+print("The new joined tuple is : {}".format(joined_tup))
